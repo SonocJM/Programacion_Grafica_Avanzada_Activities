ee3af0d541e887249a0cdd568fcd343aeec8ec35
Julio Regalado <sonxzisulishimada@gmail.com>
2026-04-21 20:10:19 -0600

    adding things to shaders

---
+++ ShaderPGA/Assets/ColorShader.shader
@@ -0,0 +1,50 @@
+Shader "Custom/ColorShader"
+{
+    Properties
+    {
+        _Color ("Color", Color) = (1,1,1,1)
+    }
+
+    SubShader
+    {
+        Tags{"RenderType" = "Transparent" "Queue" = "Transparent"}
+
+        Pass
+        {
+            Blend SrcAlpha OneMinusSrcAlpha
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv :TEXCOORD0;
+            };
+            struct v2f
+            {
+                float4 pos : SV_POSITION;
+                //float3 worldPos : TEXCOORD0;
+                float2 uv : TEXCOORD0;
+            };
+
+            float4 _Color;
+
+            v2f vert(appdata v)
+            {
+                v2f o;
+                o.pos = UnityObjectToClipPos(v.vertex);
+                //o.worldPos = mul(unity_ObjectToWorld, v.vertex).xyz;
+                o.uv = v.uv;
+                return o;
+            }
+            fixed4 frag(v2f i) : SV_TARGET
+            {
+                return _Color;
+                //return float4(i.worldPos.x, i.worldPos.y, i.worldPos.z, 1.0)
+            }
+            ENDCG
+        }
+    }
+}
---
+++ ShaderPGA/Assets/MouseColor.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class MouseColor : MonoBehaviour
+{
+    public Material material;
+    public TextMeshProUGUI textoRGB;
+    private Camera _cam;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _cam = Camera.main;
+        material = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Mouse.current == null)
+        {
+            return;
+        }
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+        Ray ray = _cam.ScreenPointToRay(mousePos);
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            Vector3 local = transform.InverseTransformPoint(hit.point);
+            float r = (local.x + 0.5f);
+            float g = (local.y + 0.5f);
+            float b = (local.z + 0.5f);
+
+            Color color = new Color(r, g, b, 1.0f);
+
+            material.SetColor("_Color", color);
+
+            if(textoRGB != null )
+            {
+                textoRGB.text = $"R:{r:F2} G:{g:F2} B:{b:F2}";
+            }
+        }
+    }
+}
